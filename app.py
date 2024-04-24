518975f69a451ca95f62bb7cf282610bf9900f0c
Will Olivera <wolivera@zircon.tech>
2024-04-24 19:01:40 -0300

    feat: completed story telling basic functionality

--- app.py
+++ app.py
@@ -1,60 +1,70 @@
 import os
 import requests
-import scipy
+from playsound import playsound
 from dotenv import load_dotenv
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration, AutoProcessor, AutoModel
+from transformers import BlipProcessor, BlipForConditionalGeneration
 from langchain_openai import ChatOpenAI
 
 # load env vars
 
 load_dotenv(".env")
 
+def get_image_description(img_url):
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+    raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+    inputs = processor(raw_image, return_tensors="pt")
+
+    out = model.generate(**inputs)
+    image_description = processor.decode(out[0], skip_special_tokens=True)
+    print("Image description: ", image_description)
+
+    return image_description
+
+def get_story(image_description):
+    llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-3.5-turbo", max_tokens=1000)
+
+    output = llm.invoke("Tell a very short story about a " + image_description)
+    story = output.content
+    print("Story: ", story)
+
+    return story
+
+def get_audio_file_inference(story):
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
+    FILE_PATH = "./story_out.flac"
+
+    headers = { "Authorization": f"Bearer {HUGGING_FACE_TOKEN}" }
+    payload = { "inputs": story }
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+    print("Response from server")
+    print(response)
+    if response.status_code == 200:
+        with open(FILE_PATH, 'wb') as file:
+            file.write(response.content)
+
+            print("Audio file generated!")
+    else:
+        print("Error from server: " + str(response.content))
+
+    return FILE_PATH
+
+def play_audio_file(file_path):
+    print('playing sound using  playsound')
+    playsound(file_path)
+
+
 # 1 Get the description for the image
-
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-
-img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
-raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-
-# conditional image captioning
-text = "describe this image with many details"
-inputs = processor(raw_image, text, return_tensors="pt")
-
-out = model.generate(**inputs, max_length=100)
-print(processor.decode(out[0], skip_special_tokens=True))
-
-# unconditional image captioning
-inputs = processor(raw_image, return_tensors="pt")
-
-out = model.generate(**inputs)
-image_description = processor.decode(out[0], skip_special_tokens=True)
-
-print("Image description: ", image_description)
-
 # 2 Create a story using the description. We'll use Langchain for this task as it offers long text generation capabilities
-
-llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-3.5-turbo", max_tokens=1000)
-
-output = llm.invoke("Tell a story about a " + image_description)
-story = output.content
-
-print("Story: ", story)
-
 # 3 Generate an audio file for the story
-processor = AutoProcessor.from_pretrained("suno/bark")
-model = AutoModel.from_pretrained("suno/bark")
-
-inputs = processor(
-    text=[story],
-    return_tensors="pt",
-)
-speech_values = model.generate(**inputs, do_sample=True)
-
-scipy.io.wavfile.write("story_out.wav", rate=22050, data=speech_values.cpu().numpy().squeeze())
-
-print("Audio file generated!")
-
 # 4 Play the audio file
-
+img_url = "https://media.cnnchile.com/sites/2/2018/06/imagen_principal-71307-720x430.jpg"
+image_description = get_image_description(img_url)
+story = get_story(image_description)
+story_out = get_audio_file_inference(story)
+play_audio_file(story_out)
