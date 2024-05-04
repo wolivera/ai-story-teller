54aaa66891a066dba51d244ad5effa5c0914c704
Will <wolivera@zircon.tech>
2024-05-03 22:53:02 -0300

    feat: streamlit setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import streamlit as st
 from playsound import playsound
 from dotenv import load_dotenv
 from PIL import Image
@@ -10,11 +11,45 @@ from langchain_openai import ChatOpenAI
 
 load_dotenv(".env")
 
-def get_image_description(img_url):
+# 1 Get the description for the image
+# 2 Create a story using the description. We'll use Langchain for this task as it offers long text generation capabilities
+# 3 Generate an audio file for the story
+# 4 Play the audio file
+# img_url = "https://media.cnnchile.com/sites/2/2018/06/imagen_principal-71307-720x430.jpg"
+# image_description = get_image_description(img_url)
+# story = get_story(image_description)
+# story_out = get_audio_file_inference(story)
+# play_audio_file(story_out)
+def main():
+    st.set_page_config(page_title="AI Story Teller", page_icon="📖", layout="centered")
+    st.title("AI Story Teller")
+    st.write("Upload an image and let us tell you a story about it")
+
+    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+    if (uploaded_image):
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+        if st.button("Tell me a story"):
+            with st.spinner("Processing image..."):
+                image_description = get_image_description(image)
+                story = get_story(image_description)
+
+                with st.expander("Image Description"):
+                    st.write(image_description)
+
+                with st.expander("Here's your personalized story:"):
+                    st.write(story)
+
+            # story_out = get_audio_file_inference(story)
+            # play_audio_file(story_out)
+
+def get_image_description(raw_image):
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-    raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+    # raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
     inputs = processor(raw_image, return_tensors="pt")
 
@@ -59,12 +94,6 @@ def play_audio_file(file_path):
     playsound(file_path)
 
 
-# 1 Get the description for the image
-# 2 Create a story using the description. We'll use Langchain for this task as it offers long text generation capabilities
-# 3 Generate an audio file for the story
-# 4 Play the audio file
-img_url = "https://media.cnnchile.com/sites/2/2018/06/imagen_principal-71307-720x430.jpg"
-image_description = get_image_description(img_url)
-story = get_story(image_description)
-story_out = get_audio_file_inference(story)
-play_audio_file(story_out)
+
+if __name__ == "__main__":
+    main()
